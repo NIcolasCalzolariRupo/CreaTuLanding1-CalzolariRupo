739f1870d6df52064a3bc2af1888ec22934dae1d
NIcolasCalzolariRupo <nicocalzolari05@gmail.com>
2025-11-07 10:57:24 -0300

    Update App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemListContainer";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer greeting="¡Bienvenido a MiTienda!" />
+    </>
+  );
+}
+
+export default App;
